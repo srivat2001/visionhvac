8478347511a1e18deacc031e8e0c56825490fbb7
srivat2001 <gsrivathsan2001@gmail.com>
2022-09-17 13:37:27 +0530

    Add all my files

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,13 +14,7 @@ const footer =()=> {
    mechanical consulting.
   </p>
 
-  <div className="social">
-    <i className="fa-brands fa-facebook-square"></i>
-    <i className="fa-brands fa-instagram-square"></i>
-    <i className="fa-brands fa-twitter-square"></i>
-    <i className="fa-brands fa-youtube-square"></i>
-    <i className="fa-brands fa-whatsapp-square"></i>
-  </div>
+
 </div>
 
 <div className="column links">
